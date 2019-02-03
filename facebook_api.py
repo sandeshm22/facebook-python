6dcf6c2f8f1f9df799128698a327d66fc06269f8
sandeshm22 <mutha.sandesh@gmail.com>
2019-02-03 15:33:42 +0530

    Update facebook_api.py

--- facebook_api.py
+++ facebook_api.py
@@ -1,33 +1,54 @@
 import facebook
 import os
-import urllib
+import requests
 
-graph = facebook.GraphAPI(access_token='EAAFzuZAjLpDABAGlzm95UD9ZBcgoKAGmYmkssoUwHyk0JR2YJrFJYKuSPxhMh8UuEYR35AulMUnjreGlHNZCv6WqXI0hN9Pis9V5YaqkAuFrpSLMXe6ORfwUiKR2S4TWCn6lKiCXQ8tBZAcvHM6Oc5Vy29W8igflmQXLKx9gitlGtHUfZAzUPSFjl2Vvjrjrf6dsSTLp6JQZDZD',version = 3.1)
-user = graph.request('/me')
-userID = user['id']
-userPhotosURL = "/"+userID+"/photos/" 
-userPhotos = graph.request(userPhotosURL)
-#print(friendsList)
-#likesData = graph.request("/+" + userID+"/likes")
-#print(likesData['data'])
-
-#likes = likesData['data']
-#for like in likes:
-   #print(like['id'])
-   #likeURL = "/" + like['id']
-   #likeData = graph.request(likeURL)
-   #print(likeData)
+def fetchfriends(myID, graphAPI):
+   myFriendsURL = "/" + myID + "/"
+   graph.request()
 
 
-for photo in userPhotos['data']:
-   #print(photo)
-   photoURL = "/" + photo['id'] + "?fields=images"
-   photoData = graph.request(photoURL)
-   #print(photoData)
-   image = photoData['images']
-   #print(image[0]['source'])
-   urllib.urlretrieve (image[0]['source'], photo['id'])
-   #xcopyCmd = "xcopy " + image[0]['source'] + " ."
-   #os.system(xcopyCmd)
-   
+def getMyPageDetails(myID, graphAPI):
+   myPages = "/" + myID + "/pages"
 
+
+def downloadfile(imageSRC, photoID):
+   image_url = imageSRC
+
+   # URL of the image to be downloaded is defined as image_url
+   r = requests.get(image_url)  # create HTTP response object
+
+   # send a HTTP request to the server and save
+   # the HTTP response in a response object called r
+   with open(photoID + ".png", 'wb') as f:
+      # Saving received content as a png file in
+      # binary format
+
+      # write the contents of the response (r.content)
+      # to a new file in binary mode.
+      f.write(r.content)
+
+def main():
+   graph = facebook.GraphAPI(access_token='EAAFzuZAjLpDABAI656GkcfAnRQmGrqJ1P0lQE3uKwpSZAlZAFUnHJJqJjyrgmPFRT9GX0UjSxDCVoYIDR6WZBa3rg2bqwyPq1rzZBitUfglGf2abmFw60o1JaV8ALzUnU92sictox3yE680vOA7UQQBzBwnEJZA1ZBQTzv0KASysHPrWvto9fcgZCyp3ZCuNixXsZD',version = 3.1)
+   user = graph.request('/me')
+   userID = user['id']
+   userPhotosURL = "/"+userID+"/photos/"
+   userPhotos = graph.request(userPhotosURL)
+   #print(friendsList)
+   #likesData = graph.request("/+" + userID+"/likes")
+   #print(likesData['data'])
+
+   #likes = likesData['data']
+   #for like in likes:
+      #print(like['id'])
+      #likeURL = "/" + like['id']
+      #likeData = graph.request(likeURL)
+      #print(likeData)
+
+
+   for photo in userPhotos['data']:
+      photoURL = "/" + photo['id'] + "?fields=images"
+      photoData = graph.request(photoURL)
+      image = photoData['images']
+      downloadfile(image[0]['source'], photo['id'])
+
+main()
